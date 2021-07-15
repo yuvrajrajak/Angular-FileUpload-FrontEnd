ea8bdcbefd6a3167503faaedf1f1e8231c140043
Yuvraj <yuvrajrajak538@gmail.com>
2021-07-15 19:42:08 +0530

    initial-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SingleFileUpload';
+  image;
+  images = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  selectImage(event) {
+    if (event.target.files.length > 0) {
+      console.log(event);
+      console.log(event.target.files);
+      // const file = event.target.files[0];
+      // this.images = file;
+      // this.images = event.target.files;
+      this.images = event.target.files;
+
+      // console.log(this.image);
+    }
+  }
+
+  onSubmit() {
+    //  const formData = new FormData();
+    //  formData.append('file',this.image);
+
+    const formData = new FormData();
+
+    for (let img of this.images) {
+      formData.append('files', img);
+    }
+
+    // this.http.post<any>('http://localhost:5000/file', formData).subscribe(
+    //   (res) => console.log(res),
+    //   (err) => console.log(err)
+    // );
+
+    this.http.post<any>('http://localhost:5000/multiple', formData).subscribe(
+      (res) => console.log(res),
+      (err) => console.log(err)
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +9,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
